16ed287d87df3a5c710986e56a75d9da4f057c3e
Jose Trujillo <jmigueltrujillo@gmail.com>
2018-07-11 09:21:37 -0600

    add cors

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ const parties = require("./routes/parties");
 const members = require("./routes/members");
 
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+    next()
+})
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 
