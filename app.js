52723d687a1bf2f5f2fb260777aa2f3ea89ed3fb
Jose Trujillo <jmigueltrujillo@gmail.com>
2018-07-11 11:38:44 -0600

    allow put

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const members = require("./routes/members");
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*')
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
     next()
 })
 app.use(morgan('dev'));
