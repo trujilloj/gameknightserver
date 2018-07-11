211ed8426258fe3cf99a987240e5efb5f6610d68
Jose Trujillo <jmigueltrujillo@gmail.com>
2018-07-11 11:06:32 -0600

    more seed data

--- parties.js
+++ parties.js
@@ -1,16 +1,17 @@
 module.exports = [{
-    id: 1,
-    dmName: "Jose",
-    partyCode: "SKT111",
-    xp: 100,
-    lastSession: "character building",
-    partyMembers: "Sean, Megan, Jared"
-},
-{
-    id: 2,
-    dmName: "Sean",
-    partyCode: "COS111",
-    xp: 100,
-    lastSession: "character building",
-    partyMembers: "Jose, Megan, David, Garrett, Bryan"
-}]
+        id: 1,
+        dmName: "Jose",
+        partyCode: "SKT111",
+        xp: 100,
+        lastSession: "character building",
+        partyMembers: "Sean, Megan, Jared"
+    },
+    {
+        id: 2,
+        dmName: "Sean",
+        partyCode: "COS111",
+        xp: 100,
+        lastSession: "Well, we 're all here and alive, but the forest seems to have gone missing. The rogue and ranger both checked for the path we came on and it seems to be missing as well. I know Axel and I didn't drink that much last night, so it 's not a case of too many libations. We'll have to pack up and go.The dark woods and looming fog dictates that we 'll need to do so without breakfast, Lathander give me strength. Not only do we have to begin without breakfast, but there was a bloody skeleton on a skeletal horse!He got spooked, the irony, and rode off before we could investigate, but he did leave behind an empty black book.The Orc, Urog took it.It 's empty so I see no harm in him holding it. I'm honestly not sure he can read... We found out what spooked the rider.We couldn 't even leave camp before that wolf Sif picked a fight with a small pack of wolves! They weren't much issue for this group, but Ghanima started going on about a huge dire wolf.No one else saw the beast, and Lathander knows that girl always has wolves on her mind, so maybe she just imagined it, but at the same time, some very strange things have occurred.The orc decided to skin the wolves against the Ranger 's wishes and is now wearing one of their pelts as a cloak. We've finally been able to pack up and have found a road in the general direction the rider looked before disappearing, and I do mean disappearing because we can 't find any horse tracks. We've decided to press on.",
+        partyMembers: "Jose, Megan, David, Garrett, Bryan"
+    }
+]
